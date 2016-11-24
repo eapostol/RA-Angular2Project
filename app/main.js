34cdfeb029a7eeca748a23af363e334f6e2a0f8d
CaitlinJD <caitlin_dyk@hotmail.com>
2016-11-24 11:05:41 -0500

    Disabled development mode

--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { platformBrowserDynamic } from
+//    '../node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js';
 "use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+var core_1 = require('@angular/core');
+core_1.enableProdMode();
+//platformBrowserDynamic().bootstrapModule(AppModule);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
-//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { platformBrowserDynamic } from
-    '../node_modules/@angular/platform-browser-dynamic/bundles';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { platformBrowserDynamic } from
+//    '../node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js';
 
 import { AppModule } from './app.module';
 
@@ -8,4 +8,4 @@ import { enableProdMode } from '@angular/core';
 
 enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(AppModule);
